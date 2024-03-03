a215fb71839f717c11cfbcf9e7f150a343116bd6
f9ki3 <floterina@gmail.com>
2024-03-03 19:22:15 +0800

    update all

--- nicole_cervantes_quiz/index.php
+++ nicole_cervantes_quiz/index.php
@@ -6,12 +6,18 @@
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
 </head>
 <body>
-<div class="container">
-    <h1 class="page-header text-center" style="font-weight: bold;">Student Quizzes</h1><p style="text-align: center">by: Nicole Cervantes</p>
-    <div class="row">
+
+
+
+<div>
+<div style=" padding: 20px; margin: 0px; ">
+        <h1 class="text-center" style="font-weight: bold;">Student Quizzes</h1>
+        <p style="text-align: center">by: Nicole Cervantes</p>
+</div>
+    
+    <div class="row" style="margin-top: 10px">
         <div class="col-sm-8 col-sm-offset-2">
-        <a href="#addnew" class="btn btn-primary" data-toggle="modal"><span class="glyphicon glyphicon-plus"></span> New</a>
-        <a href="index.php" class="btn btn-primary">Refresh</a>
+        <a href="#addnew" class="btn btn-light" style="border: 1px solid #8a8a8a;  color: #8a8a8a" data-toggle="modal"> + Add Record</a>
             <?php 
                 session_start();
                 if(isset($_SESSION['message'])){
@@ -28,13 +34,13 @@
                 <thead>
                     <th width="10%">Student No.</th>
                     <th width="15%">Student Name</th>
-                    <th width="3%">Q1</th>
-                    <th width="3%">Q2</th>
-                    <th width="3%">Q3</th>
-                    <th width="3%">Q4</th>
-                    <th width="3%">Q5</th>
-                    <th width="10%">Ave Quiz</th>
-                    <th width="20%">Action</th>
+                    <th width="5%">Quiz 1</th>
+                    <th width="5%">Quiz 2</th>
+                    <th width="5%">Quiz 3</th>
+                    <th width="5%">Quiz 4</th>
+                    <th width="5%">Quiz 5</th>
+                    <th width="10%">Average Quiz</th>
+                    <th width="15%" class="text-center">Action</th>
                 </thead>
                 <tbody>
                     <?php
@@ -44,17 +50,17 @@
                     foreach($file->user as $row){
                         ?>
                         <tr>
-                            <td><?php echo $row->id; ?></td>
-                            <td><?php echo $row->student_name; ?></td>
-                            <td><?php echo $row->q1; ?></td>
-                            <td><?php echo $row->q2; ?></td>
-                            <td><?php echo $row->q3; ?></td>
-                            <td><?php echo $row->q4; ?></td>
-                            <td><?php echo $row->q5; ?></td>
-                            <td><?php echo ($row->q1+$row->q2+$row->q3+$row->q4+$row->q5)/5; ?></td>
-                            <td>
-                                <a href="#edit_<?php echo $row->id; ?>" data-toggle="modal" class="btn btn-success btn-sm"><span class="glyphicon glyphicon-edit"></span> Edit</a>
-                                <a href="#delete_<?php echo $row->id; ?>" data-toggle="modal" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-trash"></span> Delete</a>
+                            <td class="text-center"><?php echo $row->id; ?></td>
+                            <td ><?php echo $row->student_name; ?></td>
+                            <td class="text-center"><?php echo $row->q1; ?></td>
+                            <td class="text-center"><?php echo $row->q2; ?></td>
+                            <td class="text-center"><?php echo $row->q3; ?></td>
+                            <td class="text-center"><?php echo $row->q4; ?></td>
+                            <td class="text-center"><?php echo $row->q5; ?></td>
+                            <td class="text-center"><?php echo ($row->q1+$row->q2+$row->q3+$row->q4+$row->q5)/5; ?></td>
+                            <td class="text-center">
+                                <a href="#edit_<?php echo $row->id; ?>" data-toggle="modal" class="btn btn-sm btn-warning" > Edit</a>
+                                <a href="#delete_<?php echo $row->id; ?>" data-toggle="modal" class="btn btn-sm btn-danger"> Delete</a>
                             </td>
                             <?php include('edit_delete_modal.php'); ?>
                         </tr>
